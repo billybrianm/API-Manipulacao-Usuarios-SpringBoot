3b886f206accc05c4e76eb774e676066e9c88f42
billybrian <billy.filgueira@hepta.com.br>
2021-10-04 15:47:51 -0300

    alteração nos testes unitarios

--- src/test/java/com/coodesh/billybrianm/usuariosapi/UsuariosApiApplicationTests.java
+++ src/test/java/com/coodesh/billybrianm/usuariosapi/UsuariosApiApplicationTests.java
@@ -1,7 +1,14 @@
 package com.coodesh.billybrianm.usuariosapi;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.coodesh.billybrianm.usuariosapi.model.User;
+import com.coodesh.billybrianm.usuariosapi.service.UserService;
 
 @SpringBootTest
 class UsuariosApiApplicationTests {
@@ -9,5 +16,14 @@ class UsuariosApiApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Autowired
+    private UserService userService;
 
+    @Test
+    public void testGetUsers() {
+        List<User> users= userService.list();
+
+        assertEquals(users.size(), 1);
+    }
 }
