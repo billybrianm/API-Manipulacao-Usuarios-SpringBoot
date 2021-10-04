9913bf623ebcc8d8386930b674e25b9660c7750e
billybrian <billy.filgueira@hepta.com.br>
2021-10-04 17:29:41 -0300

    inserção de novos endpoints e mudança do endereço da api

--- src/main/java/com/coodesh/billybrianm/usuariosapi/controller/UserController.java
+++ src/main/java/com/coodesh/billybrianm/usuariosapi/controller/UserController.java
@@ -11,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +22,7 @@ import com.coodesh.billybrianm.usuariosapi.model.User;
 import com.coodesh.billybrianm.usuariosapi.service.UserService;
 
 @RestController
-@RequestMapping("/api-users/users")
+@RequestMapping("/")
 public class UserController {
 	
 	private static final Logger logger = LogManager.getLogger(UserController.class);
@@ -30,6 +32,11 @@ public class UserController {
 		
 	
 	@GetMapping
+	public ResponseEntity<String> getMessage() {
+		return new ResponseEntity<>("REST Back-end Challenge 20201209 Running", HttpStatus.OK);
+	}
+	
+	@GetMapping("/users")
 	public ResponseEntity<List<User>> getAllUsers() {
 		try {
 			List<User> users = new ArrayList<User>();
@@ -49,15 +56,31 @@ public class UserController {
 		}
 	}
 	
-	@PostMapping
+	@PostMapping("/users")
 	public ResponseEntity<User> createUser(@RequestBody User user) {
 		try {
 			User _user = userService.save(user); 
 					
 					
-					
+			logger.log(Level.INFO, "Novo usuário inserido na base.");
 			return new ResponseEntity<>(_user, HttpStatus.CREATED);
 		} catch (Exception e) {
+			logger.error("Erro interno do servidor ao inserir usuário.");
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@PutMapping("/users/{id}")
+	public ResponseEntity<User> updateUser(@PathVariable("id") long id, User user) {
+		try {
+			user.setUserid(id);
+			User _user = userService.save(user);
+			
+			logger.log(Level.INFO, "Usuário atualizado com sucesso.");
+			return new ResponseEntity<>(_user, HttpStatus.OK);
+			
+		} catch (Exception e) {
+			logger.error("Erro ao atualizar o usuário recebido.");
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
