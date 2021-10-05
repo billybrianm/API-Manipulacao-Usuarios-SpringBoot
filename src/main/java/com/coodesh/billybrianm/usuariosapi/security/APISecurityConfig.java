ff703021e269e9704aa620c8ba24d1e5b9e00645
billybrian <billy.filgueira@hepta.com.br>
2021-10-05 01:20:59 -0300

    adicionados arquivos de segurança e API_KEY para o projeto

---
+++ src/main/java/com/coodesh/billybrianm/usuariosapi/security/APISecurityConfig.java
@@ -0,0 +1,49 @@
+package com.coodesh.billybrianm.usuariosapi.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+@Configuration
+@EnableWebSecurity
+@Order(1)
+public class APISecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${usuariosapi.http.auth-token-header-name}")
+    private String principalRequestHeader;
+
+    @Value("${usuariosapi.http.auth-token}")
+    private String principalRequestValue;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        APIKeyAuthFilter filter = new APIKeyAuthFilter(principalRequestHeader);
+        filter.setAuthenticationManager(new AuthenticationManager() {
+
+            @Override
+            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                String principal = (String) authentication.getPrincipal();
+                if (!principalRequestValue.equals(principal))
+                {
+                    throw new BadCredentialsException("A API Key não foi encontrada ou o valor está incorreto.");
+                }
+                authentication.setAuthenticated(true);
+                return authentication;
+            }
+        });
+        httpSecurity.
+            antMatcher("/users/**").
+            csrf().disable().
+            sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+            and().addFilter(filter).authorizeRequests().anyRequest().authenticated();
+    }
+
+}
